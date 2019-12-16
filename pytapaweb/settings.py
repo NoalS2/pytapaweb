67b56750a93207d2c15f9cab0855244e262e6e74
Noal Smith <noalssmith2@live.com>
2019-12-16 11:29:17 -0700

    added a base.html with toolbar.

--- pytapaweb/settings.py
+++ pytapaweb/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'pytapaweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['pytapaweb/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
