8f0032c5261b7702337d8ae2f114e3a4662db711
Noal Smith <noalssmith2@live.com>
2019-12-26 07:53:52 -0700

    Tapa model created and running

--- tapa/models.py
+++ tapa/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Tapa(models.Model):
     title = models.CharField(max_length=255)
-    url = models.URLField()
     pub_date = models.DateTimeField()
     image = models.ImageField(upload_to='images/')
     solution = models.CharField(max_length=255)
