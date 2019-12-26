c7f9b34e852da4068a23068342215676dea16ab6
Noal Smith <noalssmith2@live.com>
2019-12-26 07:41:37 -0700

    added in the Tapa model to models.py

--- tapa/models.py
+++ tapa/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Tapa(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.URLField()
+    pub_date = models.DateTimeField()
+    image = models.ImageField(upload_to='images/')
+    solution = models.CharField(max_length=255)
